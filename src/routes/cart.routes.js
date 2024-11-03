4939b0c46c1d51a754df3caf1d3aa099534f6db6
Ahsan280 <ahsanrehman281@gmail.com>
2024-11-03 17:06:23 +0500

    aloo commit

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -8,7 +8,7 @@ import { Router } from "express";
 
 import { isAdmin, isAuthenticated } from "../middlewares/auth.middleware.js";
 const cartRouter = Router();
-cartRouter.route("/add-to-cart").post(isAuthenticated, addToCart);
+cartRouter.route("/add-to-cart").post(addToCart);
 cartRouter.route("/get-cart").get(getCart);
 cartRouter.route("/decrement-from-cart").post(decrementFromCart);
 cartRouter.route("/remove-from-cart").post(removeFromCart);
